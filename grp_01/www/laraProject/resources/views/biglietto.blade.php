a84415afd383a5e83ec97630860f1cf2fb2b82ad
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-13 17:38:54 +0200

    Livello2

---
+++ grp_01/www/laraProject/resources/views/biglietto.blade.php
@@ -0,0 +1,34 @@
+@extends('layout.AreaUtente2')
+
+@section('title', 'Biglietti')
+<!-- inizio della sottosezione Biglietti -->
+@section('subcontent')
+<div class="ticket ticket-1">
+  
+  <div class="ticketdate">
+    <span class="ticketday">31</span>
+    <span class="ticketmonth-and-time">OCT</br><span class="ticketsmall">8PM</span></span>
+  </div>
+
+  <div class="ticketartist">
+    <span class="name">PETER TOSH</span>
+    </br>
+<span class="live small">LIVE</span>
+  </div>
+
+  <div class="ticketlocation">
+    <span>KINGSTON TOWN</span>
+    </br>
+    <span class="ticketsmall"><span>NANCY'S PUB</span>
+  </div>
+
+  <div class="ticketrip">
+  </div>
+  <div class="ticketcta">
+    <button class="ticketinfo" href="#">DETTAGLI EVENTO</button>
+  </div>
+ 
+
+</div>
+@endsection
+
