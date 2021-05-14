8c8f8a1cc8ab508dde00100abc7e3ab6d08d3284
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-14 17:51:06 +0200

    z

--- grp_01/www/laraProject/resources/views/faq.blade.php
+++ grp_01/www/laraProject/resources/views/faq.blade.php
@@ -16,6 +16,7 @@
         </div>
     </div>
 
+<<<<<<< Updated upstream
       <section class="faq">
         <ul class="categories">
           <li><a class="selected" href="#basics">Basics</a></li>
@@ -191,4 +192,42 @@
       
       
 
+=======
+    <div class="container">
+        
+        
+        <div class="accordion">
+          <div class="accordion-item">
+            <a>What can JavaScript Do?</a>
+            <div class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <a>How JavaScript Can Modify HTML and CSS Values?</a>
+            <div class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <a>What Is SVG?</a>
+            <div class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <a>Is FAQ Section Matters In Website?</a>
+            <div class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+            </div>
+          </div>
+          <div class="accordion-item">
+            <a>How To Create a Light FAQ Element?</a>
+            <div class="content">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut. Ut tortor pretium viverra suspendisse potenti.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+>>>>>>> Stashed changes
 @endsection
