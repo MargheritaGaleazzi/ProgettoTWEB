0f47c03246e026e9254600ff6d42c5eafa7e3e8f
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-05-14 10:20:07 +0200

    modifica dati

--- grp_01/www/laraProject/database/seeds/DatabaseSeeder.php
+++ grp_01/www/laraProject/database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('faq')->insert([
-            ['id_faq' => 1, 'domanda' => 'Quali sono i metodi di pagamento accettati?', 'risposta' => 'Accettiamo pagamento con carte di cedito o PayPal.'],
+            ['id_faq' => 1, 'domanda' => 'Quali sono i metodi di pagamento accettati?', 'risposta' => 'Accettiamo pagamento con carte di credito, PayPal e bonifico.'],
             ['id_faq' => 2, 'domanda' => 'Posso comprare più di un biglietto?', 'risposta' => 'Certamente, non ci sono limiti al numero di biglietti acquistabili (ovviamente fino ad esaurimento scorte).'],
             ['id_faq' => 3, 'domanda' => 'Quali sono i criteri di ricerca dei prodotti in vendita applicabili?', 'risposta' => 'Dal sito Bigliettone è possibile effettuare una ricerca semplice o ricerca avanzata. La ricerca semplice può essere effettuata utilizzando la barra di ricerca. La ricerca avanzata può essere effettuata applicando i filtri.'],
             ['id_faq' => 4, 'domanda' => 'Come faccio a registrarmi al sito?', 'risposta' => 'Nella barra di navigazione in alto basta cliccare sul link di Log-In e procedere con la registrazione guidata.'],
@@ -45,9 +45,13 @@ class DatabaseSeeder extends Seeder
         
         DB::table('evento')->insert([
             ['societa_organizzatrice' => 'PartyLove', 'prezzo_biglietto' => 35.00, 'data_ora' => '2021:08:17 20:15:00',
-                'informazioni' => 'descrizione', 'titolo' => 'FortunaLive', 'totale_biglietti_evento' => 50000, 'biglietti_rimanenti' => 50000, 
+                'informazioni' => 'Emma torna in concerto! Vieni anche tu ad ascoltarla dal vivo, una esperienza indimenticabile', 'titolo' => 'FortunaLive', 'totale_biglietti_evento' => 50000, 'biglietti_rimanenti' => 50000, 
                 'coordinate_maps' => 'https://www.google.com/maps/place/Unipol+Arena/@44.4860382,11.2481098,17z/data=!3m1!4b1!4m5!3m4!1s0x477fd68911c059af:0x84c9dbd8d81468cf!8m2!3d44.4860382!4d11.2481098',
                 'luogo' => 'Unipol Arena - CASALECCHIO DI RENO'],
+            ['societa_organizzatrice' => 'ConcertoPlanner', 'prezzo_biglietto' => 100.00, 'data_ora' => '2021:07:03 21:30:00',
+                'informazioni' => 'descrizione', 'titolo' => 'NonStopLive', 'totale_biglietti_evento' => 40000, 'biglietti_rimanenti' => 40000, 
+                'coordinate_maps' => 'https://www.google.com/maps/place/Autodromo+Enzo+e+Dino+Ferrari/@44.3440738,11.7143509,17z/data=!3m1!4b1!4m5!3m4!1s0x132b4a7474d71167:0x81177e3551c217a6!8m2!3d44.3439681!4d11.7143121',
+                'luogo' => 'Autodromo Internazionale Enzo e Dino Ferrari - IMOLA'],
             ['societa_organizzatrice' => 'ConcertoPlanner', 'prezzo_biglietto' => 100.00, 'data_ora' => '2021:07:03 21:30:00',
                 'informazioni' => 'descrizione', 'titolo' => 'NonStopLive', 'totale_biglietti_evento' => 40000, 'biglietti_rimanenti' => 40000, 
                 'coordinate_maps' => 'https://www.google.com/maps/place/Autodromo+Enzo+e+Dino+Ferrari/@44.3440738,11.7143509,17z/data=!3m1!4b1!4m5!3m4!1s0x132b4a7474d71167:0x81177e3551c217a6!8m2!3d44.3439681!4d11.7143121',
