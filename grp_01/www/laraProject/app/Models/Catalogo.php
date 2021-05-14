be18a44379f0fdf47f69d1f96f029f4679aa93d3
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-14 15:47:12 +0200

    Query Catalogo

--- grp_01/www/laraProject/app/Models/Catalogo.php
+++ grp_01/www/laraProject/app/Models/Catalogo.php
@@ -13,9 +13,10 @@ class Catalogo {
     }
     
     public function getEventiFiltrati($descr=null,$reg=null,$date=null) {
+        $dataDivisa=explode("-",$date);
         //se non specifico descrizone e regione ricerca per data
         if ((is_null($descr))&&((is_null($reg)))){
-            return Evento::where('luogo', $reg)->get()->paginate(2);
+            return Evento::where(year('data_ora'), $dataDivisa[0])->where(month('data_ora'), $dataDivisa[1])->get()->paginate(2);
         }
         //se non specifico descrizone e data ricerca per regione
         if ((is_null($descr))&&((is_null($date)))){
@@ -27,19 +28,20 @@ class Catalogo {
         }
         //se non specifico regione ricerca per descrizione e data
         if ((is_null($reg))){
-           return Evento::where()->get()->paginate(2); 
+           return Evento::where(year('data_ora'), $dataDivisa[0])->where(month('data_ora'), $dataDivisa[1])->whereIn('informazioni', $descr)->get()->paginate(2); 
         }
         //se non specifico descrizione ricerca per regione e data
         if ((is_null($descr))){
-           return Evento::where()->get()->paginate(2); 
+           return Evento::where('luogo', $reg)->where(year('data_ora'), $dataDivisa[0])->where(month('data_ora'), $dataDivisa[1])->get()->paginate(2); 
         }
         //se non specifico data ricerca per regione e descrizione
         if ((is_null($date))){
-           return Evento::where()->get()->paginate(2); 
+           return Evento::where()->get()->paginate(2);
+           Evento::where('luogo', $reg)->whereIn('informazioni', $descr)->get()->paginate(2);
         }
         //se non specifico descrizione ricerca per regione e data
         if ((!is_null($descr))&&(!is_null($reg))&&(!is_null($date))){
-           return Evento::where('luogo', $reg)->whereIn('informazioni', $descr)->get()->paginate(2); 
+           return Evento::where('luogo', $reg)->where(year('data_ora'), $dataDivisa[0])->where(month('data_ora'), $dataDivisa[1])->whereIn('informazioni', $descr)->get()->paginate(2); 
         }
     }
 }
