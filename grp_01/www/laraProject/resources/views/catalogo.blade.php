57e31f7b117b6c6c3e0e8de79afcdf3b6832736d
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-14 18:53:49 +0200

    searchbar

--- grp_01/www/laraProject/resources/views/catalogo.blade.php
+++ grp_01/www/laraProject/resources/views/catalogo.blade.php
@@ -4,6 +4,51 @@
 
 @section('content')
 <div class="container mt-5 mb-5">
+
+    <section class="search-sec">
+    <div class="container">
+        <form action="#" method="post" novalidate="novalidate">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="text" class="form-control search-slt" placeholder="Enter Search">
+                        </div>
+                      <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <input type="month" class="form-control search-slt" placeholder="Enter Search">
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <select class="form-control search-slt">
+                                <option>Choose Rating</option>
+<!--                               4 stars and up -->
+                                <option>
+                                  <a><p>&#9733; &#9733; &#9733; &#9733; &#x2606;</p></a href="#">
+                                </option>
+<!--                               3 stars and up -->
+                                <option>
+                                    <a><p>&#9733; &#9733; &#9733; &#x2606; &#x2606;</p></a href="#">
+                                </option>
+  <!--                               2 stars and up -->
+                                <option>
+                                    <a><p>&#9733; &#9733; &#x2606; &#x2606; &#x2606;</p></a href="#">
+                                </option>
+  <!--                               1 star and up -->
+                                <option>
+                                    <a><p>&#9733; &#x2606; &#x2606; &#x2606; &#x2606;</p></a href="#">
+                                </option>
+                            </select>
+                        </div>
+                        <div class="col-lg-3 col-md-3 col-sm-12 p-0">
+                            <button type="button" class="btn btn-danger wrn-btn">Search</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+</section>
+    
+    
     @isset($eventi)
     @foreach ($eventi as $evento)
     <div class="d-flex justify-content-center row">
