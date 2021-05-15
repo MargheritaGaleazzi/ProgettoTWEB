4b7916e48c774bc369bfe5a9460423047d2d9e02
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-15 08:53:20 +0200

    sistemato nel css

--- grp_01/www/laraProject/resources/views/pagination/paginator.blade.php
+++ grp_01/www/laraProject/resources/views/pagination/paginator.blade.php
@@ -30,10 +30,6 @@
         Fine
     @endif
 </div>
-<link href='https://fonts.googleapis.com/css?family=Galindo' rel='stylesheet'>
-<style>
-#pagination{
-    font-family: 'Galindo';font-size: 20px;
-}
-</style>
+
+
 @endif
