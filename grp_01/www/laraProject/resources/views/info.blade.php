191b1f1c329271a736c2573f4fe66e9b7379098a
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-16 06:46:43 +0200

    Update info.blade.php

--- grp_01/www/laraProject/resources/views/info.blade.php
+++ grp_01/www/laraProject/resources/views/info.blade.php
@@ -18,61 +18,64 @@
     <!-- Lastestnews -->
     <div class="Lastestnews blog">
         <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="titlepage">
 
-                        <span>It is a long established fact that a reader will be distracted by the readable <br>content of a page when looking at its layout. The point of using Lorem </span>
-                    </div>
-                </div>
-            </div>
             <div class="row">
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 margin">
                     <div class="news-box">
                         <figure><img src="images/1.jpg" alt="img" /></figure>
-                        <h3>Live With Music</h3>
-                        <span> March 20</span><span>Comment</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
+                        <h3>Chi siamo?</h3>
+                        <span>★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★</span>
+                        <p> Bigliettone è un sito che si occupa della compravendita di biglietti
+                        per concerti on-line. La più ampia gamma di biglietti è disponibile da noi,
+                        a partire dalla musica Classica, terminando con il Metal.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 margin">
                     <div class="news-box">
                         <figure><img src="images/2.jpg" alt="img" /></figure>
-                        <h3>Best Music</h3>
-                        <span> March 20</span><span>Comment</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
+                        <h3>Iscriversi come organizzatori:</h3>
+                        <span>★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★</span>
+                        <p> Se sei un organizzatore di eventi è vorresti sponsorizzare e vendere
+                        i tuoi biglietti sul nostro sito, ti basterà contattare
+                        l'amministratore del sito via e-mail.</p>
                     </div>
                 </div>
-                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 margin">
-                    <div class="news-box">
-                        <figure><img src="images/3.jpg" alt="img" /></figure>
-                        <h3>Live With Music</h3>
-                        <span> March 20</span><span>Comment</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
-                    </div>
-                </div>
+                
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
                     <div class="news-box">
                         <figure><img src="images/3.jpg" alt="img" /></figure>
-                        <h3>Live With Music</h3>
-                        <span> March 20</span><span>Comment</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
+                        <h3>Presentare il biglietto:</h3>
+                        <span>★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★</span>
+                        <p> Una volta effettuato l'aqcuisto insieme al biglietto le verrà assegnato
+                        un QR Code, per accedere al servizio basterà presentare in
+                        biglietteria il codice.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
                     <div class="news-box">
                         <figure><img src="images/1.jpg" alt="img" /></figure>
-                        <h3>Live With Music</h3>
-                        <span> March 20</span><span>Comment</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
+                        <h3>Stampa del biglietto:</h3>
+                        <span>★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★</span>
+                        <p> Hai fini dell'accesso al servizio, non è necessario stampare nulla, il codice QR può
+                        essere anche salvato e mostrato dal celulare.  </p>
+                    </div>
+                </div>
+                <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                    <div class="news-box">
+                        <figure><img src="images/1.jpg" alt="img" /></figure>
+                        <h3>I servizi offerti agli organizzatori:</h3>
+                        <span>★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★</span>
+                        <p> Gli organizzatori sulla nostra piattaforma possono vendere i biglietti per i loro concerti,
+                        possono gestire questi ultimi e inserirne di nuovi.</p>
                     </div>
                 </div>
                 <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
                     <div class="news-box">
                         <figure><img src="images/2.jpg" alt="img" /></figure>
-                        <h3>Best Music</h3>
-                        <span> March 20</span><span>Comment</span>
-                        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
+                        <h3>Perchè scegliere noi?</h3>
+                        <span>★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★✰★</span>
+                        <p> Per la nostra grande professionalità, la gamma di concerti offerti 
+                        e la facilità nell'effettuare l'acquisto.</p>
                     </div>
                 </div>
             </div>
