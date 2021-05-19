b2c44b2abe71437b79a0e1c79656335bdf0afa7b
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-19 16:10:25 +0200

    Update footer.blade.php

--- grp_01/www/laraProject/resources/views/layout/footer.blade.php
+++ grp_01/www/laraProject/resources/views/layout/footer.blade.php
@@ -1,11 +1,10 @@
 <footer id="footer_with_contact" style='background-color: black;height: 50%'>
-        <div class="footer">
+        <div class="footer" style="display: block; padding: 50px;">
             
-            <div style='float: left; background: black; width: 50%;padding-left: 500px;'>   
+              
                 <p style='color: white; font-size: 20px;'> La sede della nostra azienda: </p><br>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.42296634215!2d13.510551715179338!3d43.618554879122584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d7fbc71d78a41%3A0xee6105a518747120!2sVia%20Alessandro%20Zappata%2C%201%2C%2060121%20Ancona%20AN!5e0!3m2!1sen!2sit!4v1621342128488!5m2!1sen!2sit" width="400" height="240" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </div>
-            <div style='float: right; background: black; width: 50%;'>
+                    <iframe style="display: inline-block; float: left;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2888.42296634215!2d13.510551715179338!3d43.618554879122584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132d7fbc71d78a41%3A0xee6105a518747120!2sVia%20Alessandro%20Zappata%2C%201%2C%2060121%20Ancona%20AN!5e0!3m2!1sen!2sit!4v1621342128488!5m2!1sen!2sit" width="400" height="240" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            
                 <ul style='color: white; font-size: 20px;'>
                    <br><br><li style='font-weight: bold'>Bigliettone S.p.A.</li>
                             <li>Via Alessandro Zappata, 1 - 60121 Ancona (AN)</li>
@@ -13,7 +12,7 @@
                             <li>e-mail: info@bigliettoneconcerti.it</li>
                             <li>P.IVA: 0123456789</li>
                 </ul>
-            </div>
+            
                 
         </div>
 </footer>
