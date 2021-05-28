04df495b6c932ed693271a36f8bf2337f9595f4a
ChiaraAmalia <s1087171@studenti.univpm.it>
2021-05-28 09:40:51 +0200

    altre modifiche

--- grp_01/www/laraProject/app/Http/Controllers/ControllerLivello3.php
+++ grp_01/www/laraProject/app/Http/Controllers/ControllerLivello3.php
@@ -39,11 +39,9 @@ class ControllerLivello3 extends Controller {
         $validator = Validator::make($request->all(), [
             'titolo' => 'required|max:40',
             'locandina' => 'image|max:1024',
-            'biglietto_scontato' => 'required',
             'societa_organizzatrice' => 'required|max:40',
             'prezzo_biglietto' => 'required|numeric|min:0',
             'totale_biglietti_evento' => 'required|numeric|min:0',
-            'biglietti_rimanenti' => 'required|numeric|min:0',
             'coordinate_maps' => 'required|max:2500',
             'luogo' => 'required|max:100',
             'indicazioni' => 'required|max:2500',
