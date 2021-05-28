17626c7cc5c1a156939002b74031ed395904a54c
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-28 13:07:32 +0200

    Update AggiornamentoOrganizzatoreRequest.php

--- grp_01/www/laraProject/app/Http/Requests/AggiornamentoOrganizzatoreRequest.php
+++ grp_01/www/laraProject/app/Http/Requests/AggiornamentoOrganizzatoreRequest.php
@@ -5,7 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class NuovoOrganizzatoreRequest extends FormRequest {
+class AggiornamentoOrganizzatoreRequest extends FormRequest {
 
     /**
      * Determine if the user is authorized to make this request.
