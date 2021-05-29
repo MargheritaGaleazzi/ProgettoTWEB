bb86db7ecb9159f06f3ad299cebc8da17e97645d
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-29 14:07:51 +0200

    pulito footer dal css

--- grp_01/www/laraProject/resources/views/layout/footer.blade.php
+++ grp_01/www/laraProject/resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
-<footer id="footer_with_contact" style='background-color: black'>
+<footer id="footer_with_contact">
         <div class="footer" style="display: block; padding: 50px;">
-                <ul style='color: white; font-size: 20px;text-align: center'>
-                            <li style='font-weight: bold'>Bigliettone S.p.A.</li>
+                <ul>
+                            <li>Bigliettone S.p.A.</li>
                             <li>Via Alessandro Zappata, 1 - 60121 Ancona (AN)</li>
                             <li>Tel. 0712367458</li>
                             <li>e-mail: info@bigliettoneconcerti.it</li>
