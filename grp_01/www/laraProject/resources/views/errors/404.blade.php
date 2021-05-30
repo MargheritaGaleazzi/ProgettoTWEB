1337bdffff11f45900099ee15dcdc6d6083517a0
MargheritaGaleazzi <64259274+MargheritaGaleazzi@users.noreply.github.com>
2021-05-30 09:25:46 +0200

    .

--- grp_01/www/laraProject/resources/views/errors/404.blade.php
+++ grp_01/www/laraProject/resources/views/errors/404.blade.php
@@ -1,6 +1,7 @@
 <html>
   <head>
-    {{-- <link rel="stylesheet" href="css/404.css"> --}}
+    <link rel="icon" href="{!! asset('images/music-icon-ico-25.ico') !!}"/>
+     <title>Errore 404</title>
   </head>
 
   <body>
@@ -184,7 +185,7 @@
     <div class="container">
       <div class="left-section">
         <div class="inner-content">
-          <h1 class="heading">404</h1>
+          <h1 class="heading">Errore: 404</h1>
           <p class="subheading">Sembra che la pagina che stai cercando non esista più.</p>
           <p class="subheading">
             Torna alla <a href="{{ route('homePubblica') }}" title="Home">home.</a>
